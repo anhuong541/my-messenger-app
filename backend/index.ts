958c64caf28975d6646c630809d55727f3cb6c8a
huong666 <nguyenxuananhuong541@gmail.com>
2024-07-07 22:45:51 +0700

    remove socket.io

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,4 @@
-import { createServer } from "http";
 import e, { Request, Response } from "express";
-import { Server } from "socket.io";
 import cors from "cors";
 
 const app = e(); // run express
@@ -12,38 +10,39 @@ app.use(
   cors({
     origin: ["http://localhost:5173/", "http://localhost:3000/"],
     optionsSuccessStatus: 200,
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
   })
 );
 
-// socket.io
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:5173", // Adjust the origin to match your Svelte app's URL and port
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
+const Ably = require("ably");
 
-io.on("connection", (socket) => {
-  console.log("the code is running!!!");
-
-  // When a message is received, broadcast it to all clients
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-    console.log("listen to message: ", msg);
+async function publishSubscribe() {
+  // Connect to Ably with your API key
+  const ably = new Ably.Realtime(
+    "u1o7fg.leWzKQ:LSN-s392sA8ra9fQrn103SxKDmIyv8G01RFjkM4SRO0"
+  );
+  ably.connection.once("connected", () => {
+    console.log("Connected to Ably!");
   });
 
-  // Handle user disconnect
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  // Create a channel called 'get-started' and register a listener to subscribe to all messages with the name 'first'
+  const channel = ably.channels.get("get-started");
+  await channel.subscribe("first", (message: any) => {
+    console.log("Message received: " + message.data);
   });
-});
 
-// Start the server
-httpServer.listen(port, () => {
-  console.log(`Server is running port ${port}`);
-});
+  // Publish a message with the name 'first' and the contents 'Here is my first message!'
+  await channel.publish("first", "Here is my first message!");
+
+  // Close the connection to Ably after a 5 second delay
+  setTimeout(async () => {
+    ably.connection.close();
+    await ably.connection.once("closed", function () {
+      console.log("Closed the connection to Ably.");
+    });
+  }, 5000);
+}
+
+publishSubscribe();

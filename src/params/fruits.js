947f72f2879f4f927241329aaede6026a0b1df02
huong666 <nguyenxuananhuong541@gmail.com>
2024-07-11 13:21:02 +0700

    remove unused code

--- src/params/fruits.js
+++
@@ -1,5 +0,0 @@
-/** * @param {string} param * @return {param is ('apple' | 'orange')} * @satisfies {import('@sveltejs/kit').ParamMatcher} */
-export function match(param) {
-  console.log({ param });
-  return param === "apple" || param === "orange";
-}

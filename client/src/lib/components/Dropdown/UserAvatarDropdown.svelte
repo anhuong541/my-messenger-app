670a0cf465135ec4f281942e4316b1e7b989be04
huong666 <nguyenxuananhuong541@gmail.com>
2024-07-25 19:16:31 +0700

    chore: move logout function in the avatar dropdown

---
+++ client/src/lib/components/Dropdown/UserAvatarDropdown.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { signOut } from "$lib/utils/databaseAction";
+  import Icon from "@iconify/svelte";
+  import { Dropdown, DropdownItem } from "flowbite-svelte";
+
+  const dropdownOption = [
+    {
+      label: "Sign Out",
+      icon: "material-symbols:logout-rounded",
+      handler: signOut,
+    },
+  ];
+</script>
+
+<slot />
+<Dropdown classContainer="-translate-x-6 translate-y-2">
+  {#each dropdownOption as { label, icon, handler }}
+    <DropdownItem on:click={handler}>
+      <div class="flex item-center gap-2">
+        <Icon {icon} class="w-6 h-6" />
+        {label}
+      </div>
+    </DropdownItem>
+  {/each}
+</Dropdown>
--- client/src/lib/components/Layouts/Header.svelte
+++ client/src/lib/components/Layouts/Header.svelte
@@ -4,33 +4,30 @@
 
   import profile from "$lib/assets/default-avatar.webp";
   import Icon from "@iconify/svelte";
-  import { user } from "$lib/utils/firebase";
+  import { userInfo } from "$lib/utils/dataStore";
+  import UserAvatarDropdown from "../Dropdown/UserAvatarDropdown.svelte";
 </script>
 
 <!-- header -->
 <header class="border-b flex justify-between items-center px-4 w-full h-[8vh]">
   <nav class="flex">
     <ul class="flex items-center gap-8 text-lg font-medium">
-      <li><Icon icon="material-symbols:menu-rounded" class="w-12 h-12" /></li>
-      <li>Home</li>
-      <li>Service</li>
+      <!-- <li><Icon icon="material-symbols:menu-rounded" class="w-12 h-12" /></li> -->
+      <li><h1 class="font-semibold text-2xl">Chat Application</h1></li>
+      <!-- <li>Service</li> -->
     </ul>
   </nav>
   <div class="flex items-center justify-end gap-4">
-    <button class="flex rounded-full bg-gray-100 w-12 h-12">
-      <Icon icon="mi:add" class="w-6 h-6 m-auto" />
-    </button>
-    <button class="flex rounded-full bg-gray-100 w-12 h-12" on:click={signOut}>
-      <Icon icon="material-symbols:logout-rounded" class="w-6 h-6 m-auto" />
-    </button>
     <div
       class="flex items-center bg-primaryColor-500 text-white h-12 rounded-full overflow-hidden"
     >
       <p class="m-auto pl-4 pr-2">
-        {$user?.uid ?? "Loading User..."}
+        {$userInfo?.username ?? "Loading User..."}
         <!-- xem collection để đọc username -->
       </p>
-      <Avatar src={profile} class="h-12 w-12 m-auto" />
+      <UserAvatarDropdown>
+        <Avatar src={profile} class="h-12 w-12 m-auto cursor-pointer" />
+      </UserAvatarDropdown>
     </div>
   </div>
 </header>
